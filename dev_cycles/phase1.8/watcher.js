2ea1949b5942307807f989b860151019c3e9bd57
Karl N. Redman <karl.redman@gmail.com>
2017-09-14 15:22:22 -0500

    update

--- dev_cycles/phase1.8/watcher.js
+++ dev_cycles/phase1.8/watcher.js
@@ -1,40 +1,39 @@
 //file-system/watcher.js
 const fs = require('fs');
 
-let watched_file = "/home/karl/Documents/Heorot/timetrap/timetrap.db"
+var count=0;
+
+//let watched_file = "/home/karl/Documents/Heorot/timetrap/timetrap.db"
+let watched_file = "timetrap_projects"
 
 fs.watch(watched_file, function() {
-
-catcher();
-
+    catcher();
 });
 console.log("Now watching "+watched_file+" for changes...");
-
-var count=0;
 
 //function catcher(command){
 function catcher(){
 	//verify command
 
+    //incriment counter
+    count++;
+
 	//start timer
-	if ( count > 0) {
+    if ( count > 0) {
 		//the kernel emits multiple IN_MODIFY events via libuv + sometimes
 		// multiple writes occur for an action via timetrap -so we'll only
-		// report the composite within a time window of 1 second.
+		// report the composite within a (arbitrary) time window of 1 second.
         // see [fs.watch has double change events for file writes · Issue #3042 · nodejs/node](https://github.com/nodejs/node/issues/3042)
 		setTimeout(function () {
 			catch_timer(count);
-			count=0;
 		}, 1000);
-	}
-
-	//incriment counter
-	count++;
+    }
 }
 
-function catch_timer(count) {
-	if (count >= 1){
+function catch_timer() {
+	if (count > 0){
 		console.log("File "+watched_file+" just changed "+count+" times!");
+		count=0;
 	}
 }
 
